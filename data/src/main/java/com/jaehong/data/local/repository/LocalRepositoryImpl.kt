2d9599625c9cd89cb7285ea1f0532af9bf02c2e3
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-01-06 17:17:02 +0900

    feat: DateBase 기능 구형(Repository)

--- data/src/main/java/com/jaehong/data/local/repository/LocalRepositoryImpl.kt
+++ data/src/main/java/com/jaehong/data/local/repository/LocalRepositoryImpl.kt
@@ -1,15 +1,31 @@
 package com.jaehong.data.local.repository
 
 import com.jaehong.data.local.datasource.LocalDataSource
-import com.jaehong.data.mapper.Mapper.toDomain
+import com.jaehong.data.local.model.StudyEntity
+import com.jaehong.data.mapper.Mapper.dataBaseToDomain
+import com.jaehong.data.mapper.Mapper.dataToDomain
+import com.jaehong.data.mapper.Mapper.domainToDataBase
 import com.jaehong.domain.local.model.StudyInfo
+import com.jaehong.domain.local.model.StudyInfoItem
 import javax.inject.Inject
 
 class LocalRepositoryImpl @Inject constructor(
     private val dataSource: LocalDataSource
 ): com.jaehong.domain.local.repository.LocalRepository {
 
-    override suspend fun getStudyInfo(): StudyInfo {
-        return dataSource.getStudyInfo().toDomain()
+    override suspend fun getAllStudyInfo(dynastyType: String, studyType: String): StudyInfo {
+        return dataSource.getAllStudyInfo(dynastyType,studyType).dataToDomain()
+    }
+
+    override suspend fun gatStudyInfo(dynastyType: String, studyType: String): StudyInfo {
+        return dataSource.gatStudyInfo(dynastyType,studyType).dataToDomain()
+    }
+
+    override suspend fun gatMyStudyInfo(): List<StudyInfoItem> {
+        return dataSource.gatMyStudyInfo().dataBaseToDomain()
+    }
+
+    override suspend fun addMyStudyInfo(studyList: List<StudyInfoItem>) {
+        dataSource.addMyStudyInfo(studyList.domainToDataBase())
     }
 }
--- domain/src/main/java/com/jaehong/domain/local/repository/LocalRepository.kt
+++ domain/src/main/java/com/jaehong/domain/local/repository/LocalRepository.kt
@@ -1,7 +1,14 @@
 package com.jaehong.domain.local.repository
 
 import com.jaehong.domain.local.model.StudyInfo
+import com.jaehong.domain.local.model.StudyInfoItem
 
 interface LocalRepository {
-    suspend fun getStudyInfo(): StudyInfo
+    suspend fun getAllStudyInfo(dynastyType: String, studyType: String): StudyInfo
+
+    suspend fun gatStudyInfo(dynastyType: String, studyType: String): StudyInfo
+
+    suspend fun gatMyStudyInfo(): List<StudyInfoItem>
+
+    suspend fun addMyStudyInfo(studyList: List<StudyInfoItem>)
 }
