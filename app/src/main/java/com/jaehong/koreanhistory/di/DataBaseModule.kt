e3a7a1a937d3638fafbc21ae9b20699236339c16
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-01-06 17:12:48 +0900

    feat: DataBase hilt 로 주입

---
+++ app/src/main/java/com/jaehong/koreanhistory/di/DataBaseModule.kt
@@ -0,0 +1,37 @@
+package com.jaehong.koreanhistory.di
+
+import android.content.Context
+import androidx.room.Room
+import com.jaehong.data.local.databasse.KoreanHistoryDataBase
+import com.jaehong.data.local.databasse.KoreanHistoryDataBase.Companion.KOREAN_HISTORY_NAME
+import com.jaehong.data.local.databasse.dao.MyStudyDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataBaseModule {
+
+    @Singleton
+    @Provides
+    fun provideKoreanHistoryDataBase(
+        @ApplicationContext context: Context
+    ): KoreanHistoryDataBase {
+        return Room.databaseBuilder(
+            context,
+            KoreanHistoryDataBase::class.java,
+            KOREAN_HISTORY_NAME
+        ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMyStudy(
+        dataBase: KoreanHistoryDataBase
+    ): MyStudyDao = dataBase.myStudyDao()
+
+}
