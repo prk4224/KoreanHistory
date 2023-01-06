13f2f3695b6fe00dd6254e63f2f21fdb96fa3133
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-01-06 17:15:59 +0900

    feat: DataBase 기능 구현(DataSource)

--- data/src/main/java/com/jaehong/data/local/datasource/LocalDataSource.kt
+++ data/src/main/java/com/jaehong/data/local/datasource/LocalDataSource.kt
@@ -1,7 +1,14 @@
 package com.jaehong.data.local.datasource
 
+import com.jaehong.data.local.databasse.entity.MyStudyEntity
 import com.jaehong.data.local.model.StudyEntity
 
 interface LocalDataSource {
-    suspend fun getStudyInfo(): StudyEntity
+    suspend fun getAllStudyInfo(dynastyType: String, studyType: String): StudyEntity
+
+    suspend fun gatStudyInfo(dynastyType: String, studyType: String): StudyEntity
+
+    suspend fun gatMyStudyInfo(): List<MyStudyEntity>
+
+    suspend fun addMyStudyInfo(studyList: List<MyStudyEntity>)
 }
--- data/src/main/java/com/jaehong/data/local/datasource/LocalDataSourceImpl.kt
+++ data/src/main/java/com/jaehong/data/local/datasource/LocalDataSourceImpl.kt
@@ -2,18 +2,54 @@ package com.jaehong.data.local.datasource
 
 import android.content.Context
 import com.google.gson.Gson
+import com.jaehong.data.local.databasse.KoreanHistoryDataBase
+import com.jaehong.data.local.databasse.entity.MyStudyEntity
 import com.jaehong.data.local.model.StudyEntity
+import com.jaehong.data.util.Constants.FIRST_REVIEW
+import com.jaehong.data.util.Constants.ORIGIN_STUDY
+import com.jaehong.data.util.Constants.SAM_GUG
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class LocalDataSourceImpl @Inject constructor(
-    @ApplicationContext private val context: Context
-    ): LocalDataSource {
+    @ApplicationContext private val context: Context,
+    private val koreanHistoryDataBase: KoreanHistoryDataBase
+): LocalDataSource {
 
-    override suspend fun getStudyInfo(): StudyEntity {
-        val json = context.assets.open("Test.json").reader().readText()
+    override suspend fun getAllStudyInfo(dynastyType: String, studyType: String): StudyEntity {
+
+        val json = when(dynastyType) {
+            SAM_GUG -> context.assets.open("samkok_all.json").reader().readText()
+            else -> throw IllegalArgumentException("Dynasty Type Error")
+        }
+
         val data = Gson().fromJson(json, StudyEntity::class.java)
         return data
+    }
+
+    override suspend fun gatStudyInfo(dynastyType: String, studyType: String): StudyEntity {
+
+        val json = when {
+            dynastyType == SAM_GUG && studyType == ORIGIN_STUDY -> {
+                context.assets.open("samkok_all.json").reader().readText()
+            }
+            dynastyType == SAM_GUG && studyType == FIRST_REVIEW  -> {
+                context.assets.open("samkok_first_letter.json").reader().readText()
+            }
+            else -> {
+                throw IllegalArgumentException("Dynasty Type Error")
+            }
+        }
+        val data = Gson().fromJson(json, StudyEntity::class.java)
+        return data
+    }
+
+    override suspend fun gatMyStudyInfo(): List<MyStudyEntity> {
+        return koreanHistoryDataBase.myStudyDao().getMyStudyList()
+    }
+
+    override suspend fun addMyStudyInfo(studyList: List<MyStudyEntity>) {
+        koreanHistoryDataBase.myStudyDao().insertMyStudyWithListTransaction(studyList)
     }
 
 }
