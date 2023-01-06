700e2c7f59a7f84af458f0e809ca497edf4203e7
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-01-06 17:18:43 +0900

    feat: DataBase 기능 추가 (UseCase)

---
+++ domain/src/main/java/com/jaehong/domain/local/usecase/GetStudyInfoUseCase.kt
@@ -0,0 +1,26 @@
+package com.jaehong.domain.local.usecase
+
+import com.jaehong.domain.local.model.StudyInfo
+import com.jaehong.domain.local.model.StudyInfoItem
+import com.jaehong.domain.local.repository.LocalRepository
+import javax.inject.Inject
+
+class GetStudyInfoUseCase @Inject constructor(
+    private val localRepository: LocalRepository
+) {
+    suspend operator fun invoke(dynastyType: String, studyType: String): StudyInfo {
+        return localRepository.getAllStudyInfo(dynastyType,studyType)
+    }
+
+    suspend fun getStudyIngo(dynastyType: String, studyType: String): StudyInfo {
+        return localRepository.gatStudyInfo(dynastyType,studyType)
+    }
+
+    suspend fun getMyStudyInfo(): List<StudyInfoItem>{
+        return localRepository.gatMyStudyInfo()
+    }
+
+    suspend fun addMyStudyInfo(studyInfo: List<StudyInfoItem>){
+        localRepository.addMyStudyInfo(studyInfo)
+    }
+}
