6a4fadb2185150493a730c11c6ac2c32130a9fd4
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-01-06 17:14:35 +0900

    feat: DataBase Dao 추가

---
+++ data/src/main/java/com/jaehong/data/local/databasse/dao/MyStudyDao.kt
@@ -0,0 +1,26 @@
+package com.jaehong.data.local.databasse.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import com.jaehong.data.local.databasse.entity.MyStudyEntity
+
+@Dao
+interface MyStudyDao {
+
+    @Query("SELECT * FROM MY_STUDY_TABLE ")
+    suspend fun getMyStudyList(): List<MyStudyEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMyStudy(info: MyStudyEntity)
+
+    @Transaction
+    suspend fun insertMyStudyWithListTransaction(myStudyList: List<MyStudyEntity>) {
+        myStudyList.forEach {
+            insertMyStudy(it)
+        }
+    }
+}
