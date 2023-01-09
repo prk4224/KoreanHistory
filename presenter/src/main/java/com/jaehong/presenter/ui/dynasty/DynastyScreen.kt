3cd87deb28579597c5f0124a1b1f44f7a951eace
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-01-09 18:03:09 +0900

    feat: Dynasty Ui Animation 추가

--- presenter/src/main/java/com/jaehong/presenter/ui/dynasty/DynastyScreen.kt
+++ presenter/src/main/java/com/jaehong/presenter/ui/dynasty/DynastyScreen.kt
@@ -1,5 +1,8 @@
 package com.jaehong.presenter.ui.dynasty
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
@@ -7,6 +10,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,7 +22,11 @@ import com.jaehong.presenter.ui.MainActivity
 import com.jaehong.presenter.util.Constants.MY_KEYWORD
 
 @Composable
-fun DynastyScreen() {
+fun DynastyScreen(
+    dynastyViewModel: DynastyViewModel = hiltViewModel()
+) {
+    val isVisible = dynastyViewModel.isVisible.collectAsState().value
+
     Surface(modifier = Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier
@@ -26,32 +34,46 @@ fun DynastyScreen() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+
             MainActivity.dynastyList.forEach {
-                DynastyButton(title = it)
+                DynastyButton(title = it, isVisible)
             }
         }
     }
+    dynastyViewModel.startAnimation()
 }
 
 @Composable
 fun DynastyButton(
     title: String,
+    isVisible: Boolean,
     dynastyViewModel: DynastyViewModel = hiltViewModel()
 ) {
-    Button(
-        onClick = {
-            if(title == MY_KEYWORD) {
-                dynastyViewModel.onNavigateToMyStudyClicked()
-            } else {
-                dynastyViewModel.onNavigateToStudyTypeClicked(title)
-            }
-        },
-        Modifier
-            .width(250.dp)
-            .background(Color.White)
-            .padding(15.dp),
-        colors = ButtonDefaults.buttonColors(DynastyButtonColor)
+    AnimatedVisibility(
+        visible = isVisible,
+        enter = slideInHorizontally(initialOffsetX = {
+            -it
+        }),
+        exit = slideOutHorizontally(targetOffsetX = {
+            -it
+        })
     ) {
-        Text(text = title, style = Typography.bodyLarge)
+        Button(
+            onClick = {
+                if (title == MY_KEYWORD) {
+                    dynastyViewModel.onNavigateToMyStudyClicked()
+                } else {
+                    dynastyViewModel.onNavigateToStudyTypeClicked(title)
+                }
+            },
+            Modifier
+                .width(250.dp)
+                .background(Color.White)
+                .padding(15.dp),
+            colors = ButtonDefaults.buttonColors(DynastyButtonColor)
+        ) {
+            Text(text = title, style = Typography.bodyMedium)
+        }
     }
+
 }
