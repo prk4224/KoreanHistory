ebb649f067455525cf6dfe7b2e17c47896f6d13e
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-01-09 18:03:50 +0900

    feat: Dynasty Ui Animation State 추가

--- presenter/src/main/java/com/jaehong/presenter/ui/dynasty/DynastyViewModel.kt
+++ presenter/src/main/java/com/jaehong/presenter/ui/dynasty/DynastyViewModel.kt
@@ -1,15 +1,31 @@
 package com.jaehong.presenter.ui.dynasty
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.jaehong.presenter.navigation.Destination
 import com.jaehong.presenter.navigation.KoreanHistoryNavigator
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DynastyViewModel @Inject constructor(
     private val koreanHistoryNavigator: KoreanHistoryNavigator
 ): ViewModel() {
+
+    private val _isVisible = MutableStateFlow(false)
+    val isVisible = _isVisible.asStateFlow()
+
+    fun startAnimation(){
+        viewModelScope.launch {
+            delay(300)
+            _isVisible.value = true
+        }
+    }
+
     fun onBackButtonClicked() {
         koreanHistoryNavigator.tryNavigateBack()
     }
