b96ae26ed9be89f379bfc526cf89081c084bc874
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-01-13 18:48:51 +0900

    feat: Animation 추가

--- presenter/src/main/java/com/jaehong/presenter/ui/studytype/StudyTypeViewModel.kt
+++ presenter/src/main/java/com/jaehong/presenter/ui/studytype/StudyTypeViewModel.kt
@@ -2,11 +2,14 @@ package com.jaehong.presenter.ui.studytype
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.jaehong.presenter.navigation.Destination
 import com.jaehong.presenter.navigation.KoreanHistoryNavigator
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,10 +20,19 @@ class StudyTypeViewModel @Inject constructor(
 
     private val _dynastyState = MutableStateFlow("")
     val dynastyState = _dynastyState.asStateFlow()
+    private val _isVisible = MutableStateFlow(false)
+    val isVisible = _isVisible.asStateFlow()
 
     init {
         val dynastyType = savedStateHandle.get<String>(Destination.StudyType.DYNASTY_TYPE_KEY)
         _dynastyState.value = dynastyType?: throw IllegalArgumentException("Dynasty Type Error")
+    }
+
+    fun startAnimation(){
+        viewModelScope.launch {
+            delay(300)
+            _isVisible.value = true
+        }
     }
 
     fun onBackButtonClicked() {
