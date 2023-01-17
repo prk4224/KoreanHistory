41694fde09c4e1e3f39805cceacae84c2dba7c3f
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-01-17 18:24:42 +0900

    feat: Constants 추가

--- presenter/src/main/java/com/jaehong/presenter/util/Constants.kt
+++ presenter/src/main/java/com/jaehong/presenter/util/Constants.kt
@@ -1,6 +1,8 @@
 package com.jaehong.presenter.util
 
 object Constants {
+    const val GUIDE_KEY = "guide key"
+    const val USER_RULE_ALL = "user rule all"
     const val SAM_GUG = "삼국시대"
     const val SIN_LA = "통일신라 & 후삼국"
     const val GO_LYEO = "고려시대"
@@ -15,4 +17,6 @@ object Constants {
     const val REVIEW_TEXT = "복습페이지가 비었어요"
     const val PLUS_KEYWORD_TEXT = "+ 키워드 담으러 가기"
     const val MY_STUDY_TEXT = " 부족한 부분 한번 더 복습하기 ★"
+    const val GUIDE_CHECKED_TEXT = "다시 보지 않기"
+    const val GUIDE_OUT_TEXT = "공부하러 가기"
 }
