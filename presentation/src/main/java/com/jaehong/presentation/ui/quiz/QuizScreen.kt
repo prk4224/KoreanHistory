a9849179dc506c22eb525c2b32acc3301042f7c9
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-02-19 19:19:22 +0900

    feat: QuizScreen 생성

---
+++ presentation/src/main/java/com/jaehong/presentation/ui/quiz/QuizScreen.kt
@@ -0,0 +1,9 @@
+package com.jaehong.presentation.ui.quiz
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun QuizScreen() {
+
+    
+}
---
+++ presentation/src/main/java/com/jaehong/presentation/ui/quiz/QuizViewModel.kt
@@ -0,0 +1,10 @@
+package com.jaehong.presentation.ui.quiz
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class QuizViewModel: ViewModel() {
+
+
+}
