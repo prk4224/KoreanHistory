9faa6e58d792567827b9f6bf7215fd38ed7ab496
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-14 17:54:53 +0900

    feat: json -> firestore 에서 데이터 가져오기

--- data/src/main/java/com/jaehong/data/local/datasource/LocalDataSourceImpl.kt
+++ data/src/main/java/com/jaehong/data/local/datasource/LocalDataSourceImpl.kt
@@ -1,67 +1,64 @@
 package com.jaehong.data.local.datasource
 
 import android.content.Context
-import com.google.gson.Gson
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
 import com.jaehong.data.local.GuidePreference
 import com.jaehong.data.local.databasse.KoreanHistoryDataBase
 import com.jaehong.data.local.databasse.entity.MyStudyEntity
 import com.jaehong.data.local.model.StudyEntity
 import com.jaehong.data.util.Constants.GUIDE_TOKEN
+import com.jaehong.data.util.Constants.STUDY_TYPE_ALL
+import com.jaehong.data.util.Constants.STUDY_TYPE_FIRST
 import com.jaehong.data.util.enum_type.DynastyDetailType
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class LocalDataSourceImpl @Inject constructor(
     @ApplicationContext private val context: Context,
     private val koreanHistoryDataBase: KoreanHistoryDataBase,
-    private val preference: GuidePreference
+    private val preference: GuidePreference,
+    private val fireStore: FirebaseFirestore
 ): LocalDataSource {
+
+    private val TAG = "LocalDataSourceImpl"
 
     override suspend fun getAllStudyInfo(
         dynastyType: DynastyDetailType
     ): Flow<StudyEntity> = flow {
+        var items: StudyEntity? =null
 
-        val json = when(dynastyType) {
-            DynastyDetailType.SAM_GUG -> context.assets.open("samkok_all.json").reader().readText()
-            DynastyDetailType.SIN_LA -> context.assets.open("hoosamkok_all.json").reader().readText()
-            DynastyDetailType.GO_LYEO -> context.assets.open("golyeo_all.json").reader().readText()
-            DynastyDetailType.JO_SEON -> context.assets.open("joseon_all.json").reader().readText()
-            DynastyDetailType.MODERN_ONE -> context.assets.open("modern_all_1.json").reader().readText()
-            DynastyDetailType.MODERN_TWO -> context.assets.open("modern_all_2.json").reader().readText()
-            DynastyDetailType.MODERN_THREE -> context.assets.open("modern_all_3.json").reader().readText()
-            DynastyDetailType.MODERN_FOUR -> context.assets.open("modern_all_4.json").reader().readText()
-            DynastyDetailType.JAPANESE_ONE -> context.assets.open("japanese_all_1.json").reader().readText()
-            DynastyDetailType.JAPANESE_TWO -> context.assets.open("japanese_all_2.json").reader().readText()
-            DynastyDetailType.JAPANESE_THREE -> context.assets.open("japanese_all_3.json").reader().readText()
-            DynastyDetailType.CONTEMPORARY_ONE -> context.assets.open("contemporary_all_1.json").reader().readText()
-            DynastyDetailType.CONTEMPORARY_TWO -> context.assets.open("contemporary_all_2.json").reader().readText()
-            DynastyDetailType.CONTEMPORARY_THREE -> context.assets.open("contemporary_all_3.json").reader().readText()
-        }
-        emit(Gson().fromJson(json, StudyEntity::class.java))
+        fireStore.collection(STUDY_TYPE_ALL)
+            .document(getDocumentFirst(dynastyType))
+            .get()
+            .addOnSuccessListener { data ->
+                items = data.toObject<StudyEntity>()
+            }
+            .addOnFailureListener { Log.d(TAG, "$it") }
+            .await()
+
+        items?.let { emit(it) }
     }
 
     override suspend fun getStudyInfo(
         dynastyType: DynastyDetailType
     ): Flow<StudyEntity> = flow {
-        val json = when(dynastyType) {
-            DynastyDetailType.SAM_GUG -> context.assets.open("samkok_first_letter.json").reader().readText()
-            DynastyDetailType.SIN_LA  -> context.assets.open("hoosamkok_first_letter.json").reader().readText()
-            DynastyDetailType.GO_LYEO -> context.assets.open("golyeo_first_letter.json").reader().readText()
-            DynastyDetailType.JO_SEON -> context.assets.open("joseon_first_letter.json").reader().readText()
-            DynastyDetailType.MODERN_ONE -> context.assets.open("modern_first_letter_1.json").reader().readText()
-            DynastyDetailType.MODERN_TWO -> context.assets.open("modern_first_letter_2.json").reader().readText()
-            DynastyDetailType.MODERN_THREE -> context.assets.open("modern_first_letter_3.json").reader().readText()
-            DynastyDetailType.MODERN_FOUR -> context.assets.open("modern_first_letter_4.json").reader().readText()
-            DynastyDetailType.JAPANESE_ONE -> context.assets.open("japanese_first_letter_1.json").reader().readText()
-            DynastyDetailType.JAPANESE_TWO -> context.assets.open("japanese_first_letter_2.json").reader().readText()
-            DynastyDetailType.JAPANESE_THREE -> context.assets.open("japanese_first_letter_3.json").reader().readText()
-            DynastyDetailType.CONTEMPORARY_ONE -> context.assets.open("contemporary_first_letter_1.json").reader().readText()
-            DynastyDetailType.CONTEMPORARY_TWO -> context.assets.open("contemporary_first_letter_2.json").reader().readText()
-            DynastyDetailType.CONTEMPORARY_THREE -> context.assets.open("contemporary_first_letter_4.json").reader().readText()
-        }
-        emit(Gson().fromJson(json, StudyEntity::class.java))
+        var items: StudyEntity? =null
+
+        fireStore.collection(STUDY_TYPE_FIRST)
+            .document(getDocumentFirst(dynastyType))
+            .get()
+            .addOnSuccessListener { data ->
+                items = data.toObject<StudyEntity>()
+            }
+            .addOnFailureListener { Log.d(TAG, "$it") }
+            .await()
+
+        items?.let { emit(it) }
     }
 
     override suspend fun gatMyStudyInfo(): Flow<List<MyStudyEntity>> = flow {
@@ -87,4 +84,41 @@ class LocalDataSourceImpl @Inject constructor(
     }
 
 
+    private fun getDocumentAll(dynastyType: DynastyDetailType): String {
+        return when(dynastyType) {
+            DynastyDetailType.SAM_GUG -> "samkok_all"
+            DynastyDetailType.SIN_LA -> "hoosamkok_all"
+            DynastyDetailType.GO_LYEO -> "golyeo_all"
+            DynastyDetailType.JO_SEON -> "joseon_all"
+            DynastyDetailType.MODERN_ONE -> "modern_all_1"
+            DynastyDetailType.MODERN_TWO -> "modern_all_2"
+            DynastyDetailType.MODERN_THREE -> "modern_all_3"
+            DynastyDetailType.MODERN_FOUR -> "modern_all_4"
+            DynastyDetailType.JAPANESE_ONE -> "japanese_all_1"
+            DynastyDetailType.JAPANESE_TWO -> "japanese_all_2"
+            DynastyDetailType.JAPANESE_THREE -> "japanese_all_3"
+            DynastyDetailType.CONTEMPORARY_ONE -> "contemporary_all_1"
+            DynastyDetailType.CONTEMPORARY_TWO -> "contemporary_all_2"
+            DynastyDetailType.CONTEMPORARY_THREE -> "contemporary_all_3"
+        }
+    }
+
+    private fun getDocumentFirst(dynastyType: DynastyDetailType): String {
+        return when(dynastyType) {
+            DynastyDetailType.SAM_GUG -> "samkok_first_letter"
+            DynastyDetailType.SIN_LA -> "hoosamkok_first_letter"
+            DynastyDetailType.GO_LYEO -> "golyeo_first_letter"
+            DynastyDetailType.JO_SEON -> "joseon_first_letter"
+            DynastyDetailType.MODERN_ONE -> "modern_first_letter_1"
+            DynastyDetailType.MODERN_TWO -> "modern_first_letter_2"
+            DynastyDetailType.MODERN_THREE -> "modern_first_letter_3"
+            DynastyDetailType.MODERN_FOUR -> "modern_first_letter_4"
+            DynastyDetailType.JAPANESE_ONE -> "japanese_first_letter_1"
+            DynastyDetailType.JAPANESE_TWO -> "japanese_first_letter_2"
+            DynastyDetailType.JAPANESE_THREE -> "japanese_first_letter_3"
+            DynastyDetailType.CONTEMPORARY_ONE -> "contemporary_first_letter_1"
+            DynastyDetailType.CONTEMPORARY_TWO -> "contemporary_first_letter_2"
+            DynastyDetailType.CONTEMPORARY_THREE -> "contemporary_first_letter_3"
+        }
+    }
 }
