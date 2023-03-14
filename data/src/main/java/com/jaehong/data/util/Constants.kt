fbbd17b9ef4875e929eaf0655b08d97c20121679
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-14 17:57:26 +0900

    feat: Study Type 추가

--- data/src/main/java/com/jaehong/data/util/Constants.kt
+++ data/src/main/java/com/jaehong/data/util/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
     const val STRING_EMPTY = ""
     const val GUIDE_TOKEN = "guide token"
     const val NOTHING_TEXT = "Nothing"
+    const val STUDY_TYPE_ALL = "study_all"
+    const val STUDY_TYPE_FIRST = "study_first"
 }
