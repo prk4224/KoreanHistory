2bfb1eabdc2bf80a85dadee32b7a563a4458e317
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-16 20:21:24 +0900

    feat: StudyInfo Local 저장 (DB 수정)

--- data/src/main/java/com/jaehong/data/local/databasse/KoreanHistoryDataBase.kt
+++ data/src/main/java/com/jaehong/data/local/databasse/KoreanHistoryDataBase.kt
@@ -2,19 +2,25 @@ package com.jaehong.data.local.databasse
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.jaehong.data.local.databasse.converter.DateListConverters
 import com.jaehong.data.local.databasse.dao.MyStudyDao
+import com.jaehong.data.local.databasse.dao.StudyInfoDao
 import com.jaehong.data.local.databasse.entity.MyStudyEntity
+import com.jaehong.data.local.databasse.entity.StudyInfoEntity
 
 
 @Database(
     entities = [
-        MyStudyEntity::class
-    ],
-    version = 1
+        MyStudyEntity::class,
+        StudyInfoEntity::class
+    ], version = 4
 )
+@TypeConverters(DateListConverters::class)
 abstract class KoreanHistoryDataBase: RoomDatabase() {
 
     abstract fun myStudyDao(): MyStudyDao
+    abstract fun studyInfoDao(): StudyInfoDao
 
     companion object {
         const val KOREAN_HISTORY_NAME = "KoreanHistoryApp.db"
