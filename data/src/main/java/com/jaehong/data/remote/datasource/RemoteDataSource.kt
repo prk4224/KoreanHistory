160bef626489a018dc415759d76e4f2c2d789fa9
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-16 20:16:39 +0900

    feat: Remote DataSource 변경

--- data/src/main/java/com/jaehong/data/remote/datasource/RemoteDataSource.kt
+++ data/src/main/java/com/jaehong/data/remote/datasource/RemoteDataSource.kt
@@ -1,13 +1,9 @@
 package com.jaehong.data.remote.datasource
 
-import com.jaehong.data.local.databasse.entity.MyStudyEntity
 import com.jaehong.data.remote.model.StudyEntity
-import com.jaehong.data.util.enum_type.DynastyDetailType
+import com.jaehong.domain.local.model.enum_type.DynastyDetailType
 import kotlinx.coroutines.flow.Flow
 
 interface RemoteDataSource {
-    suspend fun getAllStudyInfo(dynastyType: DynastyDetailType): Flow<StudyEntity>
-
-    suspend fun getStudyInfo(dynastyType: DynastyDetailType): Flow<StudyEntity>
-
+    suspend fun getRemoteStudyInfo(dynastyType: DynastyDetailType, studyType: String): Flow<StudyEntity>
 }
