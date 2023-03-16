26671077a7ce71c554de0c957358f86bcb7cd7cf
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-16 20:23:26 +0900

    feat: Mapper 수정

--- data/src/main/java/com/jaehong/data/mapper/Mapper.kt
+++ data/src/main/java/com/jaehong/data/mapper/Mapper.kt
@@ -1,75 +1,100 @@
 package com.jaehong.data.mapper
 
 import com.jaehong.data.local.databasse.entity.MyStudyEntity
+import com.jaehong.data.local.databasse.entity.StudyInfoEntity
 import com.jaehong.data.remote.model.StudyEntity
 import com.jaehong.data.remote.model.StudyEntityItem
 import com.jaehong.data.util.Constants.NOTHING_TEXT
-import com.jaehong.data.util.enum_type.DynastyDetailType
 import com.jaehong.domain.local.model.StudyInfo
 import com.jaehong.domain.local.model.StudyInfoItem
+import com.jaehong.domain.local.model.enum_type.DynastyDetailType
 import com.jaehong.domain.local.model.enum_type.DynastyType
 
 object Mapper {
 
+    fun List<StudyInfoEntity>.dataToDomain(): List<StudyInfoItem> {
+        return this.map {
+            StudyInfoItem(
+                it.id,
+                it.detail,
+                it.king_name,
+                it.description.changeTypeListToArrayList()
+            )
+        }
+    }
+
+    fun List<StudyInfoItem>.domainToData(studyType: String): List<StudyInfoEntity> {
+        return this.map {
+            StudyInfoEntity(
+                it.id,
+                it.detail,
+                it.king_name,
+                it.description,
+                studyType
+            )
+        }
+    }
+
     fun StudyEntity.dataToDomain(): StudyInfo {
         val temp = StudyInfo(arrayListOf())
-        temp.item.addAll(this.item.map { it.dataToDomain() })
+        temp.items.addAll(this.items.map { it.dataToDomain() })
 
         return temp
     }
 
     fun StudyEntityItem.dataToDomain(): StudyInfoItem {
-        return StudyInfoItem(this.id, this.detail, this.king_name, this.description)
+        return StudyInfoItem(
+            this.id,
+            this.detail,
+            this.king_name,
+            this.description.changeTypeListToArrayList()
+        )
     }
 
-    private fun StudyInfo.checkDescription(detail: String,kingName: String): Int {
-        this.item.forEachIndexed { index, studyInfoItem ->
+
+    fun List<MyStudyEntity>.dataBaseToDomain(): StudyInfo {
+        val studyInfo = StudyInfo(arrayListOf())
+        this.forEach {
+            val index = studyInfo.checkDescription(it.detail, it.king_name)
+            if (index != -1) {
+                studyInfo.items[index].description.add(it.description)
+            } else {
+                studyInfo.items.add(it.dataBaseToDomain())
+            }
+        }
+        return studyInfo
+    }
+
+    private fun StudyInfo.checkDescription(detail: String, kingName: String): Int {
+        this.items.forEachIndexed { index, studyInfoItem ->
             if (studyInfoItem.detail == detail &&
                 studyInfoItem.king_name == kingName &&
-                studyInfoItem.king_name != NOTHING_TEXT) {
+                studyInfoItem.king_name != NOTHING_TEXT
+            ) {
                 return index
             }
         }
         return -1
     }
 
-    fun List<MyStudyEntity>.dataBaseToDomain(): StudyInfo {
-        val studyInfo = StudyInfo(arrayListOf())
-        this.forEach {
-            val index = studyInfo.checkDescription(it.detail,it.king_name)
-            if (index != -1) {
-                studyInfo.item[index].description.add(it.description)
-            } else {
-                studyInfo.item.add(it.dataBaseToDomain())
-            }
-        }
-        return studyInfo
-    }
-
     fun List<StudyInfoItem>.domainToDataBase(): List<MyStudyEntity> {
-        val studyInfo = mutableListOf<MyStudyEntity>()
+        val myStudyInfo = mutableListOf<MyStudyEntity>()
         this.forEach {
-            studyInfo.addAll(it.domainToDataBase())
+            myStudyInfo.add(it.domainToDataBase())
         }
-        return studyInfo
+        return myStudyInfo
     }
 
     fun MyStudyEntity.dataBaseToDomain(): StudyInfoItem {
         return StudyInfoItem(this.id, this.detail, this.king_name, arrayListOf(this.description))
     }
 
-    fun StudyInfoItem.domainToDataBase(): List<MyStudyEntity> {
-        val myStudyItems = mutableListOf<MyStudyEntity>()
-        this.description.forEachIndexed { index, data ->
-            val temp = MyStudyEntity(this.id + index, this.detail, this.king_name, data)
-            myStudyItems.add(temp)
-        }
-
-        return myStudyItems
+    fun StudyInfoItem.domainToDataBase(): MyStudyEntity {
+        return MyStudyEntity(this.id, this.detail, this.king_name, this.description[0])
     }
 
     fun String.checkedType(): DynastyDetailType {
-        return when(this) {
+        return when (this) {
             DynastyType.SAM_GUG.value -> DynastyDetailType.SAM_GUG
             DynastyType.SIN_LA.value -> DynastyDetailType.SIN_LA
             DynastyType.GO_LYEO.value -> DynastyDetailType.GO_LYEO
@@ -86,5 +111,13 @@ object Mapper {
             DynastyType.CONTEMPORARY.combineValue(3) -> DynastyDetailType.CONTEMPORARY_THREE
             else -> throw IllegalArgumentException("Dynasty Type Error")
         }
+    }
+
+    private fun <T> List<T>.changeTypeListToArrayList(): ArrayList<T> {
+        val arrayList = arrayListOf<T>()
+        this.forEach {
+            arrayList.add(it)
+        }
+        return arrayList
     }
 }
