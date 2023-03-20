9f743a705bda1877c703fdddcb364f5d19840e22
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-20 17:34:24 +0900

    feat: FireStore SDK 에서 Rest API 통신으로 변환(Repositiry)

--- data/src/main/java/com/jaehong/data/remote/datasource/RemoteDataSource.kt
+++ data/src/main/java/com/jaehong/data/remote/datasource/RemoteDataSource.kt
@@ -1,9 +1,13 @@
 package com.jaehong.data.remote.datasource
 
-import com.jaehong.data.remote.model.StudyEntity
+import com.jaehong.data.remote.model.RemoteData
+import com.jaehong.data.remote.model.RootField
 import com.jaehong.domain.local.model.enum_type.DynastyDetailType
+import com.jaehong.domain.local.model.result.NetworkResult
 import kotlinx.coroutines.flow.Flow
 
 interface RemoteDataSource {
-    suspend fun getRemoteStudyInfo(dynastyType: DynastyDetailType, studyType: String): Flow<StudyEntity>
+    suspend fun getRemoteStudyInfo(
+        dynastyType: DynastyDetailType,
+        studyType: String): Flow<NetworkResult<RootField>>
 }
--- domain/src/main/java/com/jaehong/domain/local/repository/RemoteRepository.kt
+++ domain/src/main/java/com/jaehong/domain/local/repository/RemoteRepository.kt
@@ -1,8 +1,9 @@
 package com.jaehong.domain.local.repository
 
-import com.jaehong.domain.local.model.StudyInfo
+import com.jaehong.domain.local.model.StudyInfoItem
+import com.jaehong.domain.local.model.result.NetworkResult
 import kotlinx.coroutines.flow.Flow
 
 interface RemoteRepository {
-    suspend fun getRemoteStudyInfo(dynastyType: String, studyType: String): Flow<StudyInfo>
+    suspend fun getRemoteStudyInfo(dynastyType: String, studyType: String): Flow<NetworkResult<List<StudyInfoItem>>>
 }
