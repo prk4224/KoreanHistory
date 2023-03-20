b39c85851e4e059c04ead8e1bfb70598c90b67c7
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-20 17:30:52 +0900

    사용하지 않는 data class 삭제

--- data/src/main/java/com/jaehong/data/remote/model/StudyEntity.kt
+++
@@ -1,5 +0,0 @@
-package com.jaehong.data.remote.model
-
-data class StudyEntity (
-    val items: List<StudyEntityItem> = listOf()
-)
--- data/src/main/java/com/jaehong/data/remote/model/StudyEntityItem.kt
+++
@@ -1,8 +0,0 @@
-package com.jaehong.data.remote.model
-
-data class StudyEntityItem(
-    val id: String = "",
-    val detail: String = "",
-    val king_name: String = "",
-    val description: List<String> = listOf(),
-)
