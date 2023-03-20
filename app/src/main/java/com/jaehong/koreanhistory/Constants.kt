811b9a276d278a90d083142cce0ed1f024d24741
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-20 17:29:32 +0900

    feat: Base URL 추가 및 Local Properties 에 id 값 저장

--- app/src/main/java/com/jaehong/koreanhistory/Constants.kt
+++ app/src/main/java/com/jaehong/koreanhistory/Constants.kt
@@ -2,4 +2,6 @@ package com.jaehong.koreanhistory
 
 object Constants {
     const val GUIDE_KEY = "guide key"
+    const val FIRE_STORE_ID = BuildConfig.FIRESTORE_ID
+    const val BASE_URL = "https://firestore.googleapis.com/v1/projects/$FIRE_STORE_ID/databases/(default)/"
 }
