75f199e0051c957c579a3e2a6bc3cd704a104eaa
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-21 20:22:31 +0900

    feat: DB Result 값으로 받아오기 구현(loading bar)

---
+++ presentation/src/main/java/com/jaehong/presentation/util/composable/CircularProgressBar.kt
@@ -0,0 +1,23 @@
+package com.jaehong.presentation.util.composable
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun CircularProgressBar() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        CircularProgressIndicator(
+            color = Color.Black,
+            modifier = Modifier
+                .align(Alignment.Center)
+        )
+    }
+}
