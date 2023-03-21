9c8fb03d06309b7c68ba77ffc25ef3a509742aab
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-21 20:18:58 +0900

    feat: DB Result 값으로 받아오기 구현(DataSource)

--- data/src/main/java/com/jaehong/data/local/datasource/LocalDataSource.kt
+++ data/src/main/java/com/jaehong/data/local/datasource/LocalDataSource.kt
@@ -1,20 +1,23 @@
 package com.jaehong.data.local.datasource
 
+import android.content.Context
+import android.net.ConnectivityManager
 import com.jaehong.data.local.databasse.entity.MyStudyEntity
 import com.jaehong.data.local.databasse.entity.StudyInfoEntity
 import com.jaehong.domain.local.model.enum_type.DynastyDetailType
+import com.jaehong.domain.local.model.result.DbResult
 import kotlinx.coroutines.flow.Flow
 
 interface LocalDataSource {
-    suspend fun getLocalStudyInfo(studyType: String): Flow<List<StudyInfoEntity>>
+    suspend fun getLocalStudyInfo(studyType: String): Flow<DbResult<List<StudyInfoEntity>>>
 
-    suspend fun insertLocalStudyIndo(studyList: List<StudyInfoEntity>)
+    suspend fun insertLocalStudyIndo(studyList: List<StudyInfoEntity>): Flow<DbResult<Boolean>>
 
-    suspend fun getMyStudyInfo(): Flow<List<MyStudyEntity>>
+    suspend fun getMyStudyInfo(): Flow<DbResult<List<MyStudyEntity>>>
 
-    suspend fun insertMyStudyInfo(studyList: List<MyStudyEntity>)
+    suspend fun insertMyStudyInfo(studyList: List<MyStudyEntity>): Flow<DbResult<Boolean>>
 
-    suspend fun deleteMyStudyInfo(studyList: List<MyStudyEntity>)
+    suspend fun deleteMyStudyInfo(studyList: List<MyStudyEntity>): Flow<DbResult<Boolean>>
 
     suspend fun getRemoteUpdateState(dynastyType: DynastyDetailType, studyType: String): Flow<Boolean>
 
--- data/src/main/java/com/jaehong/data/local/datasource/LocalDataSourceImpl.kt
+++ data/src/main/java/com/jaehong/data/local/datasource/LocalDataSourceImpl.kt
@@ -1,39 +1,73 @@
 package com.jaehong.data.local.datasource
 
-import com.jaehong.data.local.preference.KoreanHistoryPreference
 import com.jaehong.data.local.databasse.KoreanHistoryDataBase
 import com.jaehong.data.local.databasse.entity.MyStudyEntity
 import com.jaehong.data.local.databasse.entity.StudyInfoEntity
-import com.jaehong.data.util.Constants.GUIDE_TOKEN
+import com.jaehong.data.local.network_connect.NetworkManager
+import com.jaehong.data.local.preference.KoreanHistoryPreference
+import com.jaehong.data.util.networkCallback
+import com.jaehong.data.util.safeDataBaseCall
 import com.jaehong.domain.local.model.enum_type.DynastyDetailType
+import com.jaehong.domain.local.model.result.DbResult
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class LocalDataSourceImpl @Inject constructor(
     private val koreanHistoryDataBase: KoreanHistoryDataBase,
     private val preference: KoreanHistoryPreference,
-): LocalDataSource {
+    private val networkManager: NetworkManager,
+) : LocalDataSource {
 
-    override suspend fun getLocalStudyInfo(studyType: String)
-    : Flow<List<StudyInfoEntity>> = flow {
-        emit(koreanHistoryDataBase.studyInfoDao().getLocalStudyInfo(studyType))
+    override suspend fun getLocalStudyInfo(
+        studyType: String
+    ): Flow<DbResult<List<StudyInfoEntity>>> = flow {
+        emit(
+            safeDataBaseCall(
+                koreanHistoryDataBase.studyInfoDao().getLocalStudyInfo(studyType)
+            )
+        )
     }
 
-    override suspend fun insertLocalStudyIndo(studyList: List<StudyInfoEntity>) {
-        koreanHistoryDataBase.studyInfoDao().insertStudyInfoWithListTransaction(studyList)
+    override suspend fun insertLocalStudyIndo(
+        studyList: List<StudyInfoEntity>
+    ): Flow<DbResult<Boolean>> = flow {
+        emit(
+            safeDataBaseCall(
+                koreanHistoryDataBase.studyInfoDao().insertStudyInfoWithListTransaction(studyList)
+            )
+        )
     }
 
-    override suspend fun getMyStudyInfo(): Flow<List<MyStudyEntity>> = flow {
-         emit(koreanHistoryDataBase.myStudyDao().getMyStudyList())
+    override suspend fun getMyStudyInfo(): Flow<DbResult<List<MyStudyEntity>>> = flow {
+        emit(
+            safeDataBaseCall(
+                koreanHistoryDataBase.myStudyDao().getMyStudyList()
+            )
+        )
     }
 
-    override suspend fun insertMyStudyInfo(studyList: List<MyStudyEntity>) {
-        koreanHistoryDataBase.myStudyDao().insertMyStudyWithListTransaction(studyList)
+    override suspend fun insertMyStudyInfo(
+        studyList: List<MyStudyEntity>
+    ): Flow<DbResult<Boolean>> = flow {
+        emit(
+            safeDataBaseCall(
+                koreanHistoryDataBase.myStudyDao().insertMyStudyWithListTransaction(studyList)
+            )
+        )
     }
 
-    override suspend fun deleteMyStudyInfo(studyList: List<MyStudyEntity>) {
-        koreanHistoryDataBase.myStudyDao().deleteMyStudyWithListTransaction(studyList)
+    override suspend fun deleteMyStudyInfo(
+        studyList: List<MyStudyEntity>
+    ): Flow<DbResult<Boolean>> = flow {
+
+        emit(
+            safeDataBaseCall(
+                koreanHistoryDataBase.myStudyDao().deleteMyStudyWithListTransaction(studyList)
+            )
+        )
     }
 
     override suspend fun getRemoteUpdateState(
@@ -48,11 +82,11 @@ class LocalDataSourceImpl @Inject constructor(
         studyType: String,
         state: Boolean
     ) {
-        preference.setRemoteUpdateState(dynastyType.value(studyType),state)
+        preference.setRemoteUpdateState(dynastyType.value(studyType), state)
     }
 
     override suspend fun getGuideState(key: String)
-    : Flow<Boolean> = flow {
+            : Flow<Boolean> = flow {
         emit(preference.getGuideState(key))
     }
 
