f539270af8eee7e788b72014f997f028f3f9567b
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-21 20:17:07 +0900

    feat: Network Connect Check 구현(View)

--- presentation/src/main/java/com/jaehong/presentation/ui/studypage/StudyPageScreen.kt
+++ presentation/src/main/java/com/jaehong/presentation/ui/studypage/StudyPageScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.jaehong.domain.local.model.StudyInfoItem
 import com.jaehong.domain.local.model.enum_type.StudyType
+import com.jaehong.domain.local.model.result.UiStateResult
 import com.jaehong.presentation.theme.Gray2
 import com.jaehong.presentation.ui.studypage.description.DescriptionTextView
 import com.jaehong.presentation.ui.studypage.guide.blank.SelectRuleBlankDialog
@@ -16,7 +17,9 @@ import com.jaehong.presentation.ui.studypage.guide.origin.*
 import com.jaehong.presentation.ui.studypage.item.StudyAllViewItem
 import com.jaehong.presentation.ui.studypage.item.StudyPageHeaderItem
 import com.jaehong.presentation.ui.studypage.pager.StudyPagePagerScreen
+import com.jaehong.presentation.util.composable.CircularProgressBar
 import com.jaehong.presentation.util.composable.DataChangeButton
+import com.jaehong.presentation.util.dialog.NetworkConnectErrorDialog
 import com.jaehong.presentation.util.dialog.SaveCheckAlertDialog
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
@@ -25,6 +28,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 fun StudyPageScreen(
     studyPageViewModel: StudyPageViewModel = hiltViewModel()
 ) {
+    val uiState by studyPageViewModel.uiState.collectAsState()
     val dynastyType by studyPageViewModel.dynastyType.collectAsState()
     val studyType by studyPageViewModel.studyType.collectAsState()
     val firstStudyItems by studyPageViewModel.firstStudyItems.collectAsState()
@@ -37,119 +41,131 @@ fun StudyPageScreen(
     val firstGuideLabel by studyPageViewModel.firstGuideLabel.collectAsState()
     val blankGuideLabel by studyPageViewModel.blankGuideLabel.collectAsState()
     val userRuleState by studyPageViewModel.userRuleState.collectAsState()
+    val remoteState by studyPageViewModel.remoteState.collectAsState()
+    val connectionState by studyPageViewModel.connectionState.collectAsState()
 
     val selectedItems = remember { mutableStateListOf<StudyInfoItem>() }
     val snackBarState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
 
-    StudyPagePagerScreen(
-        pageList = pageList,
-        studyType = studyType,
-        isVisibleAllHint = isVisibleAllHint,
-        firstStudyItems = firstStudyItems,
-        originStudyItems = originStudyItems,
-        header = { type, title -> StudyPageHeaderItem(type, title) },
-        dynastyType = dynastyType
-    ) { studyInfo, index ->
-        StudyAllViewItem(studyInfo) { descIndex, description ->
-            val selectedItem = StudyInfoItem(
-                studyInfo.id,
-                studyInfo.detail,
-                studyInfo.king_name,
-                arrayListOf(studyInfo.description[descIndex])
-            )
+    if (remoteState || connectionState) {
+        studyPageViewModel.initStudyData()
 
-            val selected = selectedItems.contains(selectedItem)
+        StudyPagePagerScreen(
+            pageList = pageList,
+            studyType = studyType,
+            isVisibleAllHint = isVisibleAllHint,
+            firstStudyItems = firstStudyItems,
+            originStudyItems = originStudyItems,
+            header = { type, title -> StudyPageHeaderItem(type, title) },
+            dynastyType = dynastyType
+        ) { studyInfo, index ->
+            StudyAllViewItem(studyInfo) { descIndex, description ->
+                val selectedItem = StudyInfoItem(
+                    studyInfo.id,
+                    studyInfo.detail,
+                    studyInfo.king_name,
+                    arrayListOf(studyInfo.description[descIndex])
+                )
 
-            DescriptionTextView(
-                studyType = studyType,
-                selectedItem = selectedItem,
-                backgroundColor = if (selected) Gray2 else Color.White,
-                alphaText = if (selected || studyType != StudyType.ALL_BLANK_REVIEW.value) 1f else 0f,
-                hintText = if (selected) originStudyItems[index].description[descIndex] else description,
-                changeSelectedItem = { item ->
-                    if (selected) selectedItems.remove(item)
-                    else selectedItems.add(item)
-                },
-                checkedButtonState = { studyPageViewModel.checkedButtonState(selectedItems.size) },
-                changeAllHintState = { studyPageViewModel.changeAllHintState() })
+                val selected = selectedItems.contains(selectedItem)
+
+                DescriptionTextView(
+                    studyType = studyType,
+                    selectedItem = selectedItem,
+                    backgroundColor = if (selected) Gray2 else Color.White,
+                    alphaText = if (selected || studyType != StudyType.ALL_BLANK_REVIEW.value) 1f else 0f,
+                    hintText = if (selected) originStudyItems[index].description[descIndex] else description,
+                    changeSelectedItem = { item ->
+                        if (selected) selectedItems.remove(item)
+                        else selectedItems.add(item)
+                    },
+                    checkedButtonState = { studyPageViewModel.checkedButtonState(selectedItems.size) },
+                    changeAllHintState = { studyPageViewModel.changeAllHintState() })
+            }
         }
-    }
 
-    DataChangeButton(
-        iconType = true,
-        isVisible = isVisiblePlusBtn,
-        size = selectedItems.size,
-        snackBarState = snackBarState,
-        coroutineScope = coroutineScope,
-        onIconClicked = {
-            studyPageViewModel.insertMyStudyItems(selectedItems)
-            selectedItems.clear()
-        },
-        onIconLongClicked = { studyPageViewModel.onOpenDialogClicked() },
-    )
-
-    if (isVisibleDialog) {
-        SaveCheckAlertDialog(
-            dialogType = true,
-            onDialogConfirm = {
-                studyPageViewModel.onDialogConfirm()
+        DataChangeButton(
+            iconType = true,
+            isVisible = isVisiblePlusBtn,
+            size = selectedItems.size,
+            snackBarState = snackBarState,
+            coroutineScope = coroutineScope,
+            onIconClicked = {
                 selectedItems.clear()
+                studyPageViewModel.insertMyStudyItems(selectedItems)
             },
-            onDialogDismiss = {
-                studyPageViewModel.onDialogDismiss()
-            }
+            onIconLongClicked = { studyPageViewModel.onOpenDialogClicked() },
         )
-    }
 
-    when(studyType) {
-        StudyType.ORIGIN_STUDY.value -> if(originGuideLabel < 4 && userRuleState) {
-            UserRuleOriginGuide(
-                label = originGuideLabel,
-                swipe = {
-                    SwipeRuleOriginDialog { label, type ->
-                        studyPageViewModel.updateLabel(label, type)
-                    }
+        if (isVisibleDialog) {
+            SaveCheckAlertDialog(
+                dialogType = true,
+                onDialogConfirm = {
+                    studyPageViewModel.onDialogConfirm()
+                    selectedItems.clear()
                 },
-                scroll = {
-                    ScrollRuleOriginDialog { label, type ->
-                        studyPageViewModel.updateLabel(label, type)
-                    }
-                },
-                select = {
-                    SelectRuleOriginDialog { label, type ->
-                        studyPageViewModel.updateLabel(label, type)
-                    }
-                },
-                allSave = {
-                    AllSaveRuleOriginDialog { label, type, rule ->
-                        studyPageViewModel.updateLabel(label, type)
-                        studyPageViewModel.updateUserRule(rule)
-                    }
+                onDialogDismiss = {
+                    studyPageViewModel.onDialogDismiss()
                 }
             )
         }
-        StudyType.FIRST_REVIEW.value -> if(firstGuideLabel < 2 && userRuleState) {
-            UserRuleFirstGuide(
-                firstGuideLabel,
-                select = {
-                    SelectRuleFirstDialog { label, type ->
-                        studyPageViewModel.updateLabel(label, type)
+
+        when (studyType) {
+            StudyType.ORIGIN_STUDY.value -> if (originGuideLabel < 4 && userRuleState) {
+                UserRuleOriginGuide(
+                    label = originGuideLabel,
+                    swipe = {
+                        SwipeRuleOriginDialog { label, type ->
+                            studyPageViewModel.updateLabel(label, type)
+                        }
+                    },
+                    scroll = {
+                        ScrollRuleOriginDialog { label, type ->
+                            studyPageViewModel.updateLabel(label, type)
+                        }
+                    },
+                    select = {
+                        SelectRuleOriginDialog { label, type ->
+                            studyPageViewModel.updateLabel(label, type)
+                        }
+                    },
+                    allSave = {
+                        AllSaveRuleOriginDialog { label, type, rule ->
+                            studyPageViewModel.updateLabel(label, type)
+                            studyPageViewModel.updateUserRule(rule)
+                        }
                     }
-                },
-                longClick = {
-                    LongClickRuleFirstDialog { label, type, rule ->
-                        studyPageViewModel.updateLabel(label, type)
-                        studyPageViewModel.updateUserRule(rule)
+                )
+            }
+            StudyType.FIRST_REVIEW.value -> if (firstGuideLabel < 2 && userRuleState) {
+                UserRuleFirstGuide(
+                    firstGuideLabel,
+                    select = {
+                        SelectRuleFirstDialog { label, type ->
+                            studyPageViewModel.updateLabel(label, type)
+                        }
+                    },
+                    longClick = {
+                        LongClickRuleFirstDialog { label, type, rule ->
+                            studyPageViewModel.updateLabel(label, type)
+                            studyPageViewModel.updateUserRule(rule)
+                        }
                     }
+                )
+            }
+            StudyType.ALL_BLANK_REVIEW.value -> if (blankGuideLabel && userRuleState) {
+                SelectRuleBlankDialog { label, type, rule ->
+                    studyPageViewModel.updateLabel(label, type)
+                    studyPageViewModel.updateUserRule(rule)
                 }
-            )
-        }
-        StudyType.ALL_BLANK_REVIEW.value -> if(blankGuideLabel && userRuleState) {
-            SelectRuleBlankDialog { label, type, rule ->
-                studyPageViewModel.updateLabel(label, type)
-                studyPageViewModel.updateUserRule(rule)
             }
         }
+
+        if (uiState == UiStateResult.LOADING) {
+            CircularProgressBar()
+        }
+    } else {
+        NetworkConnectErrorDialog()
     }
 }
